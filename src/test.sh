957da5f71358d5c0a30a97436b94422591397949
Mazen Abdelgawad <mazenabdelgawad700@gmail.com>
2026-03-09 17:39:22 +0200

    update test.sh file

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,5 @@
 EXPECTED= "Hello, Test!"
-OUTPUT= $(node -e  "console.log(require(./src/app)('Test'))")
+OUTPUT= $(node -e  "console.log(require('./src/app')('Test'))")
 
 if ["$OUTPUT" == "$EXPECTED"]; then
 
